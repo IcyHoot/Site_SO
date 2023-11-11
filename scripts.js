e3a5b0313e5e9f84635a0e04329f1ae40d4bebd8
Ryan Vieira <108676672+IcyHoot@users.noreply.github.com>
2023-11-11 10:34:16 -0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,7 @@
+var som95 = new Audio("src/msc/Windows/Win95.mp3");
+var botao = document.getElementById("Sound95");
+
+
+var somxp = new Audio("src/msc/Windows/WinXP.mp3");
+var botao = document.getElementById("SoundXP");
+som95.volume(0.2)
